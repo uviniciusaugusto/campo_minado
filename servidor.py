1b904e4c6969733f65c972fff1bcdf99d06ebc8e
Vinicius Augusto <uviniciusaugusto@gmail.com>
2021-03-29 19:28:44 -0300

    add cliente, update servidor

--- servidor.py
+++ servidor.py
@@ -2,9 +2,44 @@ import random
 import socket
 import json
 
+#Função responsavel por montar a matriz do campo minado
+def gerarCampoMinado(g, b):
+    campos = [[0 for row in range(g)] for column in range(g)]
+    for num in range(b):
+        x = random.randint(0,g-1)
+        y = random.randint(0,g-1)
+        campos[y][x] = -1
+        if (x >=0 and x <= g-2) and (y >= 0 and y <= g-1):
+            if campos[y][x+1] != -1:
+                campos[y][x+1] += 1 
+        if (x >=1 and x <= g-1) and (y >= 0 and y <= g-1):
+            if campos[y][x-1] != -1:
+                campos[y][x-1] += 1 
+        if (x >= 1 and x <= g-1) and (y >= 1 and y <= g-1):
+            if campos[y-1][x-1] != -1:
+                campos[y-1][x-1] += 1 
+ 
+        if (x >= 0 and x <= g-2) and (y >= 1 and y <= g-1):
+            if campos[y-1][x+1] != -1:
+                campos[y-1][x+1] += 1
+        if (x >= 0 and x <= g-1) and (y >= 1 and y <= g-1):
+            if campos[y-1][x] != -1:
+                campos[y-1][x] += 1 
+ 
+        if (x >=0 and x <= g-2) and (y >= 0 and y <= g-2):
+            if campos[y+1][x+1] != -1:
+                campos[y+1][x+1] += 1 
+        if (x >= 1 and x <= g-1) and (y >= 0 and y <= g-2):
+            if campos[y+1][x-1] != -1:
+                campos[y+1][x-1] += 1 
+        if (x >= 0 and x <= g-1) and (y >= 0 and y <= g-2):
+            if campos[y+1][x] != -1:
+                campos[y+1][x] += 1
+    return campos
+
 serverPort = 50000
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-serverSocket.bind(('',serverPort))
+serverSocket.bind(("127.0.0.1",serverPort))
 serverSocket.listen(0)
 
 jogadores = [];
@@ -19,25 +54,29 @@ rodadas = 0
 print("Servidor pronto para recever")
 
 while True:
+    #Recebe a conexão
     connectionSocket, addr = serverSocket.accept()
-    print("Conexão vinda de {}".format(addr))
     message = connectionSocket.recv(2048)
-    print ("{} ==> {}".format(addr, message.decode("utf-8")))
     idJogador = format(addr)
-    dados = json.loads(message.decode("utf-8"))
+    #Decodifica a mensagem
+    mensagem = message.decode("utf-8")
+  
+    dados = json.loads(mensagem)
 
+    if(dados['fim'] == True):
+        break;
+    
+    #Acrescenta os dados dos jogadores nas estruturas
     if (idJogador not in jogadores):
         jogadores.append(idJogador)
-        pontuacoes[idJogador] = 0
-        json[idJogador] = {}
-        json[idJogador]['pontos'] = 0
-        json[idJogador]['conexao'] = connectionSocket
+        pontuacoes.append(0)
 
+        jsonJogadores[idJogador] = {}
+    
     if(dados['reiniciar'] == True):
         campos = []
     
     if(dados['iniciar'] == True or len(campos)<1):
-        
         tamGrid = dados['dificuldade']
 
         grid = 0
@@ -58,28 +97,20 @@ while True:
     
     elif(dados['linha'] != -1):
         rodadas += 1
-        linha = (dados['linha']);
-        coluna = (dados['coluna']);
-
-        if(campos[linha][coluna] != 'B'):
-            json[idJogador]['pontos'] += 1
-            i = 0
-            while i < len(jogadores): 
-                if(jogadores[i] == idJogador):
-                    pontuacoes[i] = json[idJogador]['pontos']
+        linha = int(dados['linha']);
+        coluna = int(dados['coluna']);
+        
+        if(campos[linha][coluna] != -1):
+            pontuacoes[0] += 1
         
         coord = [];
         coord.append(linha);
         coord.append(coluna);
         posicoes.append(coord);
-        i = 0
-        ranking = sorted(pontuacoes, reverse=True)
-        while(i < len(pontuacoes)):
-            if(pontuacoes[idJogador]==ranking[i]):
-                colocacao = i;
-                break;
+        colocacao = 1
         
         resposta = {
+            "pontuacao" : pontuacoes[0],
             "posicoes" : posicoes,
             "valor": campos[linha][coluna],
             "qtdJogadores" : len(jogadores),
@@ -91,52 +122,21 @@ while True:
         resposta = {
             "posicoes" : posicoes,
         }
-    jsonJogadores[idJogador]['resposta'] = resposta
+    jsonJogadores['resposta'] = resposta
     
+
+    #Definição de qual jogador deverá jogar
     rodada += 1
     if(rodada >= len(jogadores)):
         rodada = 0
-  
     for j in jogadores:
-        jsonJogadores[j]['suaVez'] == False
-    jsonJogadores[rodada]['suaVez'] == True
+        jsonJogadores[j]['suaVez'] = False
+    jsonJogadores[jogadores[rodada]]['suaVez'] = True
     
-    for j in jogadores:
-        jsonJogadores[j]['conexao'].send(jsonJogadores.encode("utf-8"))
+    #Conversão e envio dos dados
+    message = json.dumps(jsonJogadores)
+    connectionSocket.send(message.encode("utf-8"))
 
-for j in jogadores:
-    jsonJogadores[j]['conexao'].close()
+    #Fechamento da conexão
+    connectionSocket.close()
 
-def gerarCampoMinado(g, b):
-    campos = [[0 for row in range(g)] for column in range(g)]
-    for num in range(b):
-        x = random.randint(0,g-1)
-        y = random.randint(0,g-1)
-        arr[y][x] = 'B'
-        if (x >=0 and x <= g-2) and (y >= 0 and y <= g-1):
-            if campos[y][x+1] != 'B':
-                campos[y][x+1] += 1 
-        if (x >=1 and x <= g-1) and (y >= 0 and y <= g-1):
-            if campos[y][x-1] != 'B':
-                campos[y][x-1] += 1 
-        if (x >= 1 and x <= g-1) and (y >= 1 and y <= g-1):
-            if campos[y-1][x-1] != 'B':
-                campos[y-1][x-1] += 1 
- 
-        if (x >= 0 and x <= g-2) and (y >= 1 and y <= g-1):
-            if campos[y-1][x+1] != 'B':
-                campos[y-1][x+1] += 1
-        if (x >= 0 and x <= g-1) and (y >= 1 and y <= g-1):
-            if campos[y-1][x] != 'B':
-                campos[y-1][x] += 1 
- 
-        if (x >=0 and x <= g-2) and (y >= 0 and y <= g-2):
-            if campos[y+1][x+1] != 'B':
-                campos[y+1][x+1] += 1 
-        if (x >= 1 and x <= g-1) and (y >= 0 and y <= g-2):
-            if campos[y+1][x-1] != 'B':
-                campos[y+1][x-1] += 1 
-        if (x >= 0 and x <= g-1) and (y >= 0 and y <= g-2):
-            if campos[y+1][x] != 'B':
-                campos[y+1][x] += 1
-    return campos
